9210787b8708a04f037b9fa9cf7ddf0d75531675
nastyahh <nasstyahus@gmail.com>
2024-04-24 19:54:29 +0300

    create FAB

--- app/src/main/java/ui/components/screens/NotesScreen.kt
+++ app/src/main/java/ui/components/screens/NotesScreen.kt
@@ -5,9 +5,14 @@ import androidx.compose.runtime.Composable
 import com.topic2.android.notes.viewmodel.MainViewModel
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.rememberScaffoldState
 
@@ -35,7 +40,20 @@ fun NotesScreen(
 
     val coroutineScope = rememberCoroutineScope()
 
-    Scaffold (topBar={
+    Scaffold (floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                viewModel.onCreateNewNoteClick() },
+                contentColor = MaterialTheme.colors.background,
+                content = {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "Add Note Button"
+                    )
+                }
+            )
+        },
+        topBar={
         TopAppBar(
             title = "Заметки",
             icon = Icons.Filled.List,
